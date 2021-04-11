93a5d239a1ca76d7b7017eb8907be152921e598b
sivarakeshreddy123 <kxsuneetha834@gmail.com>
2021-04-11 17:57:59 +0530

    Update App.js
    Changes message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there!, How Are You..Thank You!!</p>
+        <p>Hi there!,Changed From feature Branch..!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
